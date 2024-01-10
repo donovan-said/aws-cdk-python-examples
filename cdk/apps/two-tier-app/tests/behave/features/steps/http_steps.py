b8b8dfb25bdd2e267a42f34a2ced588c13765fe7
Donovan Said <donovan.s.said@gmail.com>
2024-01-10 16:39:34 +0000

    Continued dev on behave tests.

--- cdk/apps/two-tier-app/tests/behave/features/steps/http_steps.py
+++ cdk/apps/two-tier-app/tests/behave/features/steps/http_steps.py
@@ -1,1 +1,27 @@
 #!/usr/bin/env python3
+
+"""
+The following is a suite of behave steps for HTTP connection tests
+"""
+
+from behave import given, when, then
+
+
+@given("TBC")
+def step(context):
+    pass
+
+
+@when("TBC")
+def step(context):
+    pass
+
+
+@when("TBC")
+def step(context):
+    pass
+
+
+@then("TBC")
+def step(context, status_code):
+    pass
